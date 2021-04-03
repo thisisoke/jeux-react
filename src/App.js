60d889a9a146bd78bb1d1b369f2ca55eb3925de2
thisioke <seyitanoke@gmail.com>
2021-04-03 01:45:58 -0400

    Added React router dependecy. Pulled in boiler plate to app.js space index.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+import { Login } from './screen/Login.jsx';
+
+
+export function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    //Router navigates the "page" in the broswer search bar to the correct screen component as listed below
+
+   //Switch ensures that each page component is displayed on its own. And the URL has to be an 'exact match'
+   <Router>
+     
+     <Switch>
+       <Route path="/" exact component={Login}/>
+      
+     </Switch>
+
+   </Router>
+
+ );
 }
 
-export default App;
