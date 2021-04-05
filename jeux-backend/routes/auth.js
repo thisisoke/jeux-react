76127d01e1a1915c6eb5194488d611b59372f332
thisioke <seyitanoke@gmail.com>
2021-04-05 15:50:27 -0400

    Added bcrypt. Application can now hash paswords and store encrypted passwords. Only User Id is returned

--- jeux-backend/routes/auth.js
+++ jeux-backend/routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../model/User');
+const bcrypt = require('bcryptjs');
 const {registerValidation} = require('../validation');
 
 
@@ -14,16 +15,20 @@ router.post('/register', async (req,res) => {
     //Check if The user is already in the Database
     const emailExist = await User.findOne({email: req.body.email});
     if(emailExist) return res.status(400).send('Email already exisits')
+
+    //Hash Paasswords
+    const salt = await bcrypt.genSalt(10);
+    const hashPassword = await bcrypt.hash(req.body.password, salt);
    
     //Create a new User
     const user = new User({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password
+        password: hashPassword
     });
     try{
         const savedUser = await user.save();
-        res.send(savedUser);
+        res.send({user: user.id});
     }catch(err){
         res.status(400).send(err);
     }
