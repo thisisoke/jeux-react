75d06b82965524bde7ce543d8dee56e0414125ac
thisioke <seyitanoke@gmail.com>
2021-04-05 15:14:47 -0400

    validation works now. user request body will be validated first before it is sent to back end

--- jeux-backend/routes/auth.js
+++ jeux-backend/routes/auth.js
@@ -14,21 +14,20 @@ const validSchema = Joi.object({
 router.post('/register', async (req,res) => {
 
     //Validating Data from User Before User is made
-    const validation = validSchema.validate(req.body);
-    res.send(validation);
-
-    // const user = new User({
-    //     name: req.body.name,
-    //     email: req.body.email,
-    //     password: req.body.password
-    // });
-    // try{
-    //     const savedUser = await user.save();
-    //     res.send(savedUser);
-    // }catch(err){
-    //     res.status(400).send(err);
-    //     console.log(err);
-    // }
+    const {error} = validSchema.validate(req.body);
+    if(error) return res.status(400).send(error.details[0].message);
+   
+    const user = new User({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+    });
+    try{
+        const savedUser = await user.save();
+        res.send(savedUser);
+    }catch(err){
+        res.status(400).send(err);
+    }
 
 });
 
