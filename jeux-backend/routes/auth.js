0744291af2331dadcbe2a6431de21bde7268756b
thisioke <seyitanoke@gmail.com>
2021-04-05 15:07:20 -0400

    Validation with Joi. Used new way of validation not the same as DEVED's version

--- jeux-backend/routes/auth.js
+++ jeux-backend/routes/auth.js
@@ -2,20 +2,33 @@ const router = require('express').Router();
 
 const User = require('../model/User');
 
+//Validation
+const Joi = require('@hapi/joi');
+const validSchema = Joi.object({
+    name: Joi.string().min(6).required(),
+    email: Joi.string().min(6).required().email(),
+    password: Joi.string().min(6).required()
+});
+
 
 router.post('/register', async (req,res) => {
-    const user = new User({
-        name: req.body.name,
-        email: req.body.email,
-        password: req.body.password
-    });
-    try{
-        const savedUser = await user.save();
-        res.send(savedUser);
-    }catch(err){
-        res.status(400).send(err);
-        console.log(err);
-    }
+
+    //Validating Data from User Before User is made
+    const validation = validSchema.validate(req.body);
+    res.send(validation);
+
+    // const user = new User({
+    //     name: req.body.name,
+    //     email: req.body.email,
+    //     password: req.body.password
+    // });
+    // try{
+    //     const savedUser = await user.save();
+    //     res.send(savedUser);
+    // }catch(err){
+    //     res.status(400).send(err);
+    //     console.log(err);
+    // }
 
 });
 
